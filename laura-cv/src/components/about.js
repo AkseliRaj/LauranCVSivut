9aeff8c8f9ca1fc6abff114918785cd5939afb78
AkseliRaj <akseli.rajahalme@gmail.com>
2025-12-04 21:20:04 +0200

    Added hero section to about.js

--- laura-cv/src/components/about.js
+++ laura-cv/src/components/about.js
@@ -2,28 +2,35 @@ import React from 'react';
 import '../css/about.css';
 
 import profilePicture from '../assets/webp/profilePicture.webp'
+import lampIcon from '../assets/svg/lamp.svg';
+import locationIcon from '../assets/svg/location.svg';
 
 export default function About() {
     return (
         <div className="about-container container-fluid">
             <div className='row justify-content-center align-items-center py-5'>
-                
+
+                {/*HERO SECTION*/}
                 <div className='col-xl-10 col-md-12'>
                     <h1>LAURA WALDEN *</h1>
                 </div>
 
                 <div className='col-xl-5 col-md-6'>
-                    <img className='profilePicture' src={profilePicture} alt="Profile picture"/>
+                    <img className='profilePicture img-fluid' src={profilePicture} alt="Profile picture" />
                 </div>
 
                 <div className='col-xl-5 col-md-6'>
-                    <p>Lighting designer</p>
-                    <p>Based in Helsinki, Finland</p>
+                    <div className="d-flex align-items-center gap-2">
+                        <img src={lampIcon} alt="Lamp icon" />
+                        <p className="mb-0">Lighting designer</p>
+                    </div>
+
+                    <div className="d-flex align-items-center gap-2 mt-5">
+                        <img src={locationIcon} alt="Location icon" />
+                        <p className="mb-0">Based in Helsinki, Finland</p>
+                    </div>
                 </div>
 
-                <div className='col-xl-5 col-md-6'>
-                    <p>More text</p>
-                </div>
 
             </div>
         </div>
