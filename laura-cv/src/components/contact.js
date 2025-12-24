1a6383e46c9e55214e680066df83da3b205762e8
AkseliRaj <akseli.rajahalme@gmail.com>
2025-12-24 17:08:13 +0200

    Refactor contact page layout; update heading and contact details structure

--- laura-cv/src/components/contact.js
+++ laura-cv/src/components/contact.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import emailIcon from '../assets/svg/email.svg';
 import phoneIcon from '../assets/svg/phone.svg';
@@ -16,71 +15,82 @@ export default function Contact() {
   return (
     <div className="contact-container container-fluid">
 
-      {/* HEADER */}
+      {/* PAGE CONTENT WRAPPER */}
       <div className="row justify-content-center">
-        <div className="col-11 pb-3">
-          <h1 className="contactPageHeading">Contact me!</h1>
-        </div>
+        <div className="col-11">
 
-        {/* HERO SECTION */}
-        <div className="row justify-content-center">
-          <div className="col-11 heroSection">
-            <div className="row align-items-center">
-
-              {/* LEFT: CONTACT DETAILS */}
-              <div className="col-12 col-lg-7 d-flex py-2 py-lg-5">
-                <div className="contactInformationTextWrapper">
-
-                  {/* EMAIL */}
-                  <div className="row pb-2 pb-lg-4 emailContact">
-                    <div className="col-auto d-flex align-items-center">
-                      <img
-                        src={emailIcon}
-                        alt="Email icon"
-                        className="email-icon"
-                      />
-                    </div>
-                    <div className="col d-flex align-items-center">
-                      <p>laura.em.walden@gmail.com</p>
-                    </div>
-                  </div>
-
-                  {/* PHONE */}
-                  <div className="row pb-5 phoneNumber">
-                    <div className="col-auto d-flex align-items-center">
-                      <img
-                        src={phoneIcon}
-                        alt="Phone icon"
-                        className="phone-icon"
-                      />
-                    </div>
-                    <div className="col d-flex align-items-center">
-                      <p>+358 045-272 0123</p>
-                    </div>
-                  </div>
-
-                </div>
-              </div>
-
-              {/* RIGHT: CV DOWNLOAD BUTTON */}
-              <div className="col-5 d-none d-lg-flex justify-content-center">
-                <button
-                  type="button"
-                  className="btn btn-primary"
-                  data-bs-toggle="modal"
-                  data-bs-target="#cvDownloadModal"
-                >
-                  Download CV
-                </button>
-              </div>
-
+          {/* HEADER */}
+          <div className="row pb-3">
+            <div className="col">
+              <h1 className="contactPageHeading">Contact</h1>
             </div>
           </div>
-        </div>
 
-        {/* CONTACT FORM */}
-        <div className="contactForm col-11">
-          <ContactForm />
+          {/* HERO SECTION */}
+          <div className="row justify-content-center">
+            <div className="col-12 heroSection">
+
+              <div className="row align-items-center">
+
+                {/* CONTACT DETAILS */}
+                <div className="col-12 col-md-8 py-2 py-lg-5">
+                  <div className="contactInformationTextWrapper">
+
+                    {/* EMAIL */}
+                    <div className="row pb-2 pb-lg-4 emailContact">
+                      <div className="col-auto d-flex align-items-center">
+                        <img
+                          src={emailIcon}
+                          alt="Email icon"
+                          className="email-icon"
+                        />
+                      </div>
+                      <div className="col d-flex align-items-center">
+                        <p>laura.em.walden@gmail.com</p>
+                      </div>
+                    </div>
+
+                    {/* PHONE */}
+                    <div className="row pb-4 phoneNumber">
+                      <div className="col-auto d-flex align-items-center">
+                        <img
+                          src={phoneIcon}
+                          alt="Phone icon"
+                          className="phone-icon"
+                        />
+                      </div>
+                      <div className="col d-flex align-items-center">
+                        <p>+358 045-272 0123</p>
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+
+                {/* DOWNLOAD CV BUTTON */}
+                <div className="col-12 col-md-4 pb-5 pb-md-0 d-flex justify-content-md-end justify-content-start align-items-center">
+                  <button
+                    type="button"
+                    className="btn btn-primary"
+                    data-bs-toggle="modal"
+                    data-bs-target="#cvDownloadModal"
+                  >
+                    Download CV
+                  </button>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+
+          {/* CONTACT FORM */}
+          <div className="row justify-content-center">
+            <div className="col-12 contactForm">
+              <ContactForm />
+            </div>
+          </div>
+
         </div>
       </div>
 
